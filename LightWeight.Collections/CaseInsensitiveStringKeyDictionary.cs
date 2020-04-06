9d06fe379711d36e3b4da4f792042ea712d8eb0f
Lengyel János Zoltán <lengyel.janos@outlook.com>
2020-04-06 14:46:08 +0200

    optimize case insensitive dictionaries

--- LightWeight.Collections/CaseInsensitiveStringKeyDictionary.cs
+++ LightWeight.Collections/CaseInsensitiveStringKeyDictionary.cs
@@ -1,25 +1,26 @@
 ﻿namespace FizzCode.LightWeight.Collections
 {
+    using System;
     using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
 
     public class CaseInsensitiveStringKeyDictionary<T> : IEnumerable<T>
     {
-        private readonly Dictionary<string, T> _itemsByUpperKey = new Dictionary<string, T>();
+        private readonly Dictionary<string, T> _items = new Dictionary<string, T>(StringComparer.InvariantCultureIgnoreCase);
 
         public IReadOnlyList<T> GetItemsAsReadonly()
         {
-            return _itemsByUpperKey.Values.ToList().AsReadOnly();
+            return _items.Values.ToList().AsReadOnly();
         }
 
-        public int Count => _itemsByUpperKey.Count;
+        public int Count => _items.Count;
 
         public T this[string key]
         {
             get
             {
-                _itemsByUpperKey.TryGetValue(key.ToUpperInvariant(), out var item);
+                _items.TryGetValue(key, out var item);
                 return item;
             }
             set => Add(key, value);
@@ -27,32 +28,32 @@
 
         public bool ContainsKey(string key)
         {
-            return _itemsByUpperKey.ContainsKey(key.ToUpperInvariant());
+            return _items.ContainsKey(key);
         }
 
         public void Add(string key, T value)
         {
-            _itemsByUpperKey[key.ToUpperInvariant()] = value;
+            _items[key] = value;
         }
 
         public void Remove(string key)
         {
-            _itemsByUpperKey.Remove(key.ToUpperInvariant());
+            _items.Remove(key);
         }
 
         public void Clear()
         {
-            _itemsByUpperKey.Clear();
+            _items.Clear();
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            return _itemsByUpperKey.Values.GetEnumerator();
+            return _items.Values.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return _itemsByUpperKey.Values.GetEnumerator();
+            return _items.Values.GetEnumerator();
         }
     }
 }
--- LightWeight.Collections/OrderedCaseInsensitiveStringKeyDictionary.cs
+++ LightWeight.Collections/OrderedCaseInsensitiveStringKeyDictionary.cs
@@ -1,5 +1,6 @@
 ﻿namespace FizzCode.LightWeight.Collections
 {
+    using System;
     using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
@@ -7,20 +8,20 @@
     public class OrderedCaseInsensitiveStringKeyDictionary<TItem> : IEnumerable<TItem>
     {
         private readonly List<Entry> _entriesOrdered = new List<Entry>();
-        private readonly Dictionary<string, Entry> _entriesByUpperKey = new Dictionary<string, Entry>();
+        private readonly Dictionary<string, Entry> _entries = new Dictionary<string, Entry>(StringComparer.InvariantCultureIgnoreCase);
 
         public IReadOnlyList<TItem> GetItemsAsReadonly()
         {
             return _entriesOrdered.Select(x => x.Item).ToList().AsReadOnly();
         }
 
-        public int Count => _entriesByUpperKey.Count;
+        public int Count => _entries.Count;
 
         public TItem this[string key]
         {
             get
             {
-                if (_entriesByUpperKey.TryGetValue(key.ToUpperInvariant(), out var entry))
+                if (_entries.TryGetValue(key, out var entry))
                     return entry.Item;
 
                 return default;
@@ -31,13 +32,12 @@
         public void Clear()
         {
             _entriesOrdered.Clear();
-            _entriesByUpperKey.Clear();
+            _entries.Clear();
         }
 
         public void Add(string key, TItem item)
         {
-            var upperKey = key.ToUpperInvariant();
-            if (_entriesByUpperKey.TryGetValue(upperKey, out var entry))
+            if (_entries.TryGetValue(key, out var entry))
             {
                 entry.Item = item;
             }
@@ -46,19 +46,17 @@
                 entry = new Entry()
                 {
                     Key = key,
-                    UpperKey = upperKey,
                     Item = item,
                 };
 
                 _entriesOrdered.Add(entry);
-                _entriesByUpperKey[upperKey] = entry;
+                _entries[key] = entry;
             }
         }
 
         public bool Insert(string key, TItem item, int index)
         {
-            var upperKey = key.ToUpperInvariant();
-            if (_entriesByUpperKey.ContainsKey(upperKey))
+            if (_entries.ContainsKey(key))
                 return false;
 
             if (index < 0 || index > _entriesOrdered.Count)
@@ -67,23 +65,19 @@
             var entry = new Entry()
             {
                 Key = key,
-                UpperKey = upperKey,
                 Item = item,
             };
 
             _entriesOrdered.Insert(index, entry);
-            _entriesByUpperKey[upperKey] = entry;
+            _entries[key] = entry;
             return true;
         }
 
         public void Remove(string key)
         {
-            var upperKey = key.ToUpperInvariant();
-            if (_entriesByUpperKey.TryGetValue(upperKey, out var item))
+            if (_entries.Remove(key))
             {
-                _entriesByUpperKey.Remove(upperKey);
-
-                var index = _entriesOrdered.FindIndex(x => x.UpperKey == upperKey);
+                var index = _entriesOrdered.FindIndex(x => _entries.Comparer.Equals(x.Key, key));
                 _entriesOrdered.RemoveAt(index);
             }
         }
@@ -101,7 +95,6 @@
         private class Entry
         {
             public string Key { get; set; }
-            public string UpperKey { get; set; }
             public TItem Item { get; set; }
         }
     }
