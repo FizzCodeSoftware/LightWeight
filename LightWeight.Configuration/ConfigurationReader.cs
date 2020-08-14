533d7aa75a5b65f8cd76b96580296c7033deefcb
Lengyel János Zoltán <lengyel.janos@outlook.com>
2020-08-14 16:17:45 +0200

    support direct section reading in ConfigurationReader

--- LightWeight.Configuration/ConfigurationReader.cs
+++ LightWeight.Configuration/ConfigurationReader.cs
@@ -37,5 +37,38 @@
 
             return value;
         }
+
+        public static T GetCurrentValue<T>(IConfigurationSection section, string key, T defaultValue)
+        {
+            var value = section.GetValue<T>(key + "-" + Environment.MachineName, default);
+            if (value != null && !value.Equals(default(T)))
+                return value;
+
+            return section.GetValue(key, defaultValue);
+        }
+
+        public static string GetCurrentValue(IConfigurationSection section, string key, string defaultValue, IConfigurationSecretProtector protector = null)
+        {
+            var isProtected = section.GetValue(key + "-protected", false);
+
+            var value = section.GetValue<string>(key + "-" + Environment.MachineName, default);
+            if (value != null)
+            {
+                if (isProtected && protector != null)
+                {
+                    value = protector.Decrypt(value);
+                }
+
+                return value;
+            }
+
+            value = section.GetValue(key, defaultValue);
+            if (value != null && isProtected && protector != null)
+            {
+                value = protector.Decrypt(value);
+            }
+
+            return value;
+        }
     }
 }
