a1a30b11f3b874d5a2063f3526e91472e552c49e
Lengyel János Zoltán <lengyel.janos@gmail.com>
2024-11-11 09:41:02 +0100

    FormatException

--- LightWeight/ExceptionExtensions.cs
+++ LightWeight/ExceptionExtensions.cs
@@ -2,7 +2,7 @@
 
 public static class ExceptionExtensions
 {
-    public static string FormatExceptionWithDetails(this Exception exception, bool includeTrace = true)
+    public static string FormatException(this Exception exception, bool includeTrace = true)
     {
         try
         {
@@ -16,72 +16,6 @@ public static class ExceptionExtensions
                     msg += "\nINNER EXCEPTION: ";
 
                 msg += cex.GetType().GetFriendlyTypeName() + ": " + cex.Message;
-
-                if (cex.Data?["ProcessType"] is string processType)
-                {
-                    msg += "\n\tPROCESS: ";
-
-                    if (cex.Data?["ProcessTypeAssembly"] is string processTypeAssembly)
-                        msg += "(" + processTypeAssembly + ") ";
-
-                    msg += processType;
-
-                    if (cex.Data?["ProcessName"] is string processName && processName != processType)
-                        msg += " (\"" + processName + "\")";
-
-                    if (cex.Data?["ProcessKind"] is string processKind)
-                        msg += ", kind: " + processKind;
-                }
-
-                if (cex.Data?.Count > 0)
-                {
-                    var first = true;
-                    foreach (var key in cex.Data.Keys)
-                    {
-                        var k = key.ToString();
-                        if (k is "ProcessName" or "ProcessKind" or "ProcessType" or "ProcessTypeAssembly" or "CallChain" or "OpsMessage" or "Trace" or "Row")
-                            continue;
-
-                        if (k.Contains("Row", StringComparison.InvariantCultureIgnoreCase) && cex.Data[key] is string rowStr && rowStr.StartsWith("id", StringComparison.InvariantCultureIgnoreCase))
-                            continue;
-
-                        if (first)
-                        {
-                            msg += "\n\tDATA:";
-                            first = false;
-                        }
-                        else
-                        {
-                            msg += ", ";
-                        }
-
-                        var value = cex.Data[key];
-                        msg += "\n\t\t[" + k + "] = " + (value != null ? value.ToString().Trim() : "NULL");
-                    }
-                }
-
-                if (cex.Data?["Row"] is string storedRow)
-                {
-                    msg += "\n\tROW: " + storedRow.Replace("\n", "\n\t\t", StringComparison.InvariantCultureIgnoreCase);
-                }
-
-                if (cex.Data?.Count > 0)
-                {
-                    foreach (var key in cex.Data.Keys)
-                    {
-                        var k = key.ToString();
-                        if (k == "Row")
-                            continue;
-
-                        if (k.Contains("Row", StringComparison.InvariantCultureIgnoreCase) && cex.Data[key] is string rowStr && rowStr.StartsWith("id", StringComparison.InvariantCultureIgnoreCase))
-                        {
-                            msg += "\n\t" + k.ToUpperInvariant() + ": " + rowStr;
-                        }
-                    }
-                }
-
-                if (cex.Data?["CallChain"] is string callChain)
-                    msg += "\n\tCALL CHAIN:\n\t\t" + callChain.Replace("\n", "\n\t\t", StringComparison.Ordinal);
 
                 if (includeTrace)
                 {
